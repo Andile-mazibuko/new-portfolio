9f885491bc9386fa33adeb2c53ad16cf16e74a6c
Andile <andilelethumusa@gmail.com>
2025-06-01 20:16:19 +0200

    main/home page styling

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { MainComponent } from './components/main/main.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: ' ', component: MainComponent },
+  { path: '', component: MainComponent },
   { path: '**', component: NotFoundComponent },
 ];
