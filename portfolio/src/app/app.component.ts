cbbdcbe119bf3459784fa4f7ee820f6b627c51c1
Andile <andilelethumusa@gmail.com>
2025-06-01 18:32:59 +0200

    Theme Service

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ portfolio/src/app/services/theme.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  themeSubject = new BehaviorSubject<boolean>(true);
+
+  changeTheme() {
+    this.themeSubject.next(!this.themeSubject.value);
+  }
+  
+  getTheme() {
+    return this.themeSubject.asObservable;
+  }
+}
