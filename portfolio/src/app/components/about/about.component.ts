93dbdbc9a38342bb899e4d15750de00865008d5d
Andile <andilelethumusa@gmail.com>
2025-06-10 20:06:17 +0200

    Css for tablets

--- portfolio/src/app/components/about/about.component.ts
+++ portfolio/src/app/components/about/about.component.ts
@@ -3,18 +3,19 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { CardSkill } from '../../models/skill';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [MatCardModule, MatIconModule, CommonModule],
+  imports: [MatCardModule, MatIconModule, CommonModule, MatTabsModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
 export class AboutComponent implements OnInit {
   skills: CardSkill[] = [];
   smallDescription =
-    'dadsadsadsad asddfsfcsaa dsad sfd asdd   sadsadad dsad sd s a  ad adasd dsadsad s ad adadsd';
+    'Full Stack Developer passionate about building modern web experiences';
   ngOnInit(): void {
     this.skills = [
       {
