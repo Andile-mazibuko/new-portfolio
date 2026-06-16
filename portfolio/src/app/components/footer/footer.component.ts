e7a9a0c82506129062c985abf244a96d2d1afdea
Andile <andilelethumusa@gmail.com>
2026-06-16 13:05:22 +0200

    fixed the footer issue

--- portfolio/src/app/components/footer/footer.component.ts
+++ portfolio/src/app/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SocialsComponent } from "../socials/socials.component";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [SocialsComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
